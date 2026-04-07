6ded193488843ef56a37ed01d9c399b9fb12c1ac
nobuaki109 <nobuaki2003109@gmail.com>
2026-04-07 15:51:50 +0900

    feat: add message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_07_063954) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_07_065047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.string "author_name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -20,4 +29,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_07_063954) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
 end
