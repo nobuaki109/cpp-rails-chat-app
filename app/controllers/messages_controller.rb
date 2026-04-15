87e6f16a8fec52ab7bb116ea1ab5531af342c9eb
nobuaki109 <nobuaki2003109@gmail.com>
2026-04-16 02:15:30 +0900

    feat: add moderation check to message creation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,13 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.build(message_params)
 
+    if ModerationService.contains_ng_word?(@message.content)
+      @message.errors.add(:content, "にNGワードが含まれています")
+      @messages = @room.messages.order(created_at: :asc)
+      render "rooms/show", status: :unprocessable_entity
+      return
+    end
+
     if@message.save
       redirect_to room_path(@room),notion:"メッセージを送信しました。"
     else
