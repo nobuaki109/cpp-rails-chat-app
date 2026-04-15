8ea261ac3f726e7ea1ea80e080667e5ffabf1239
nobuaki109 <nobuaki2003109@gmail.com>
2026-04-15 21:58:22 +0900

    feat: implement message posting in room detail

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+  def create
+    @room = Room.find(paramas[:room_id])
+    @message = @room.messages.build(message_params)
+
+    if@message.save
+      redirect_to room_path(@room),notion:"メッセージを送信しました。"
+    else
+      @message = @room.message.order(create_at: :asc)
+      render "rooms/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    paramas.require(:message).permit(:author_name, :content)
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/create'
   root "rooms#index"
 
-  resources :rooms, only: [:index, :show, :new, :create]
+  resources :rooms, only: [:index, :show, :new, :create] do
+    resources :messages, only: [:create]
+  end
 end
